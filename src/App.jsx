06c7ee85cb865616ad3f9dea8b54da90c965e152
Alireza Raei <alirezaraei17@gmail.com>
2024-07-18 13:05:20 +1000

    feat: Integrate Form component and enhance Map interaction
    - Add Form component for city data input.
    - Implement Form component routing in App.jsx.
    - Create Form.module.css for styling the Form component.
    - Enhance Map component with navigation to the Form page on click.

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import AppLayout from './pages/AppLayout';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
+import Form from './components/Form';
 
 const URL = 'http://localhost:3001';
 
@@ -57,7 +58,7 @@ function App() {
             path='countries'
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path='form' element={<div>Form</div>} />
+          <Route path='form' element={<Form />} />
         </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
