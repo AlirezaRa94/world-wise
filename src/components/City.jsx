f00a9784c89b71e86ce92da67d6b452bbb7f1c63
Alireza Raei <alirezaraei17@gmail.com>
2024-07-18 12:55:01 +1000

    feat: Enhance City detail view with dynamic map positioning
    - Update City component to include latitude and longitude in the display.
    - Modify CityItem to pass latitude and longitude as URL search parameters.
    - Adjust Map component to extract and display latitude and longitude from URL search parameters.

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,13 +1,19 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 import styles from './City.module.css';
 
 function City() {
   const { cityId } = useParams();
+  const [searchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
 
   return (
     <div className={styles.city}>
       <h2>{cityId}</h2>
+      <p>
+        Position: {lat}, {lng}
+      </p>
     </div>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,18 @@
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-  return <div className={styles.mapContainer}></div>;
+  const [searchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div className={styles.mapContainer}>
+      <p>
+        Positon: {lat}, {lng}
+      </p>
+    </div>
+  );
 }
 
 export default Map;
