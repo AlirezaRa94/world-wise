7583e3d8b58f2e1c8bb46c26b666419a13661bc4
Alireza Raei <alirezaraei17@gmail.com>
2024-07-19 23:50:58 +1000

    refactor: Integrate currentCity context in CityItem for active styling
    - Added useCities hook to access currentCity from CitiesContext.
    - Adjusted className logic to highlight the active city item based on currentCity.id.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,7 +1,8 @@
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import styles from './CityItem.module.css';
-import { Link } from 'react-router-dom';
+import { useCities } from '../contexts/CitiesContext';
 
 CityItem.propTypes = {
   city: propTypes.object.isRequired,
@@ -15,13 +16,16 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
 
   return (
     <li>
       <Link
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          currentCity.id === id ? styles['cityItem--active'] : ''
+        }`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
