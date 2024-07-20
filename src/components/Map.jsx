4ff2d6b775098e788f1fc87e62ffbb287eca40d5
Alireza Raei <alirezaraei17@gmail.com>
2024-07-20 10:35:50 +1000

    feat: Integrate react-leaflet for dynamic map display in Map component
    - Replaced static map display with dynamic MapContainer from react-leaflet.
    - Updated Leaflet CSS import in index.css to version 1.9.4 to match package version.
    - Added useState hook for managing map position state in Map component.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,33 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 import styles from './Map.module.css';
 
 function Map() {
-  const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams] = useSearchParams();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      <p>
-        Positon: {lat}, {lng}
-      </p>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
